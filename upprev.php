a62074622c6ddb07af1c212bc00d1cc86c40dfcc
iworks <iworks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-17 15:47:20 +0000

    #TAG 3.3.2
    git-svn-id: https://plugins.svn.wordpress.org/upprev/tags/3.3.2@506612 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- upprev.php
+++ upprev.php
@@ -3,7 +3,7 @@
 Plugin Name: upPrev
 Plugin URI: http://iworks.pl/upprev/
 Description: When scrolling post down upPrev will display a flyout box with a link to the previous post from the same category. Based on upPrev Previous Post Animated Notification by Jason Pelker, Grzegorz Krzyminski
-Version: trunk
+Version: 3.3.2
 Author: Marcin Pietrzak
 Author URI: http://iworks.pl/
 Licence: GPL2
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 /**
  * static options
  */
-define( 'IWORKS_UPPREV_VERSION', 'trunk' );
+define( 'IWORKS_UPPREV_VERSION', '3.3.2' );
 define( 'IWORKS_UPPREV_PREFIX',  'iworks_upprev_' );
 
 /**
