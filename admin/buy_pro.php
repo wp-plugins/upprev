e856de37e5057e610d108c5ef0bd6cd335998214
iworks <iworks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-02 22:27:59 +0000

    #IMPROVMENT benefits
    git-svn-id: https://plugins.svn.wordpress.org/upprev/trunk@620532 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/buy_pro.php
+++ admin/buy_pro.php
@@ -4,27 +4,27 @@ global $iworks_upprev;
 ob_start(); 
 
 ?>
+<div style="width:50%;float:right;">
+
+
 <h2><?php _e( 'Buy Pro Version', 'iworks_upprev' ); ?></h2>
 
 <h4><?php _e( 'Single site licence', 'iworks_upprev' ); ?></h4>
+
 <dl>
     <dt><?php _e( 'Price', 'iworks_upprev'); ?></dt>
     <dd><b>$40</b> - <a href="<?php $iworks_upprev->link_buy('this-site'); ?>">buy now for this site</a> or <a href="">buy for other</a></dd>
 </dl>
 
-<h4><?php _e( '3-pack licence', 'iworks_upprev' ); ?></h4>
-<dl>
-    <dt><?php _e( 'Price', 'iworks_upprev'); ?></dt>
-    <dd><b>$90</b> - 3 sites</dd>
-</dl>
-
-<h4><?php _e( 'Developer pack licence', 'iworks_upprev' ); ?></h4>
-<dl>
-    <dt><?php _e( 'Price', 'iworks_upprev'); ?></dt>
-    <dd><b>$200</b> - 10 sites</dd>
-</dl>
-
-</dl>
+</div>
+<div style="width: 49%; float: left">
+<h2><?php _e( 'Benefits', 'iworks_upprev' ); ?></h2>
+<ul class="ul-square">
+    <li><?php _e( 'All templates available.', 'iworks_upprev' ); ?></li>
+    <li><?php _e( 'All positions available.', 'iworks_upprev' ); ?></li>
+    <li><?php _e( 'You can set up colors.', 'iworks_upprev' ); ?></li>
+    <li><?php _e( 'You can exclude categories.', 'iworks_upprev' ); ?></li>
+</div>
 <?php
 $content = ob_get_contents();
 ob_end_clean();
